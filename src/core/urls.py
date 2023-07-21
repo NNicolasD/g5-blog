3df72b265d16be2a9ae1f197eb5ed609f0503a8a
Lucas Gabriel Gómez <lucasgabrielg2001@gmail.com>
2023-07-21 12:22:47 -0300

    Añadiendo una página básica de contacto

--- src/core/urls.py
+++ src/core/urls.py
@@ -6,5 +6,6 @@ app_name = 'blog'
 urlpatterns = [
     path('', InicioView.as_view(), name='home'),
     path('about/', AcercaDeNosotrosView.as_view(), name='about'),
+    path('contact/', ContactoView.as_view(), name='contact'),
     path('article/', include('articulos.urls')),
 ]
--- src/core/views.py
+++ src/core/views.py
@@ -1,7 +1,16 @@
 from django.views import generic
+from django.contrib import messages
+from django.shortcuts import redirect
 
 class InicioView(generic.TemplateView):
     template_name = 'blog/home.html'
     
 class AcercaDeNosotrosView(generic.TemplateView):
     template_name = 'blog/about.html'
+    
+class ContactoView(generic.TemplateView):
+    template_name = 'blog/contact.html'
+
+    def post(self, request, *args, **kwargs):
+        messages.success(request, 'Mensaje enviado con éxito.')
+        return redirect('blog:contact')
