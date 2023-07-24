b7af8980cea9c955224c0d9a6f8624870290c812
Lucas Gabriel Gómez <lucasgabrielg2001@gmail.com>
2023-07-24 19:10:41 -0300

    Modificando el inicio

--- src/core/views.py
+++ src/core/views.py
@@ -1,9 +1,15 @@
 from django.views import generic
 from django.contrib import messages
 from django.shortcuts import redirect
+from articulos.models import Articulo
 
 class InicioView(generic.TemplateView):
     template_name = 'blog/home.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['articulos'] = Articulo.objects.order_by('-creacion')[:3]
+        return context
     
 class AcercaDeNosotrosView(generic.TemplateView):
     template_name = 'blog/about.html'
