fb08dd895f196cb8b410cd9c00e984ff9f831fb2
Lucas Gabriel Gómez <lucasgabrielg2001@gmail.com>
2023-07-28 18:59:48 -0300

    Modificando el footer en artículos

--- src/proyectoBlog/settings/base.py
+++ src/proyectoBlog/settings/base.py
@@ -66,7 +66,7 @@ ROOT_URLCONF = 'proyectoBlog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates'),],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,9 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-    ]
+    BASE_DIR / 'static',
+]
 
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
@@ -132,4 +133,4 @@ if not DEBUG:
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR),'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
